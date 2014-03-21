20af4f4c5d5ccef2d958dce9744ba96a774f3c56
Florian Margaine <florian@margaine.com>
2014-03-22 00:18:20 +0100

    Getting the first line gives the route

--- socket.lisp
+++ socket.lisp
@@ -3,8 +3,10 @@
 (in-package #:soalh)
 
 (defun socket-start ()
-  (usocket:socket-server "0.0.0.0" 1234 #'socket-listener))
+  (usocket:socket-server "0.0.0.0" 1234 #'socket-listener '() :in-new-thread nil))
 
 (defun socket-listener (stream)
   (declare (type stream stream))
-  (format stream "Hello world!"))
+  (let ((line (read-line stream)))
+    (format t "~A~%" line))
+  (format stream "Hello, world!"))
